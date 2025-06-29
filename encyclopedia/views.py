2af0d3e638862a21004a61a725e539bf5f3ac994
Sahalu Aminu <sahlminkok7@gmail.com>
2025-06-29 10:01:26 +0100

    Create entry page view

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,8 +2,18 @@ from django.shortcuts import render
 
 from . import util
 
-
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+def entry_page(request, title):
+    entry = util.get_entry(title)
+
+    if entry is None:
+        return render(request, "encyclopedia/404.html", status=404)
+    
+    return render(request, "encyclopedia/entry.html", {
+        "entry": entry,
+        "title": title
     })
